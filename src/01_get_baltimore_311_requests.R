a6f32a9fed4a4c03f4441fcf13394ea7d50dff38
oonuttlet <77589230+oonuttlet@users.noreply.github.com>
2026-01-27 17:39:54 -0500

    started data collection

---
+++ src/01_get_baltimore_311_requests.R
@@ -0,0 +1,30 @@
+library(arcgislayers)
+library(sf)
+library(tidyverse)
+
+# get all 3-1-1 complaints from Baltimore REST
+where_clause <- "SRType IN ('SW-Dirty Alley', 'SW-Dirty Street')"
+# where_clause <- "SRType IN ('SW-Rat Rubout', 'HCD-Rodent')"
+# 2025
+cfs_2025 <- arc_read(r"(https://services1.arcgis.com/UWYHeuuJISiGmgXx/arcgis/rest/services/311_Customer_Service_Requests_2025/FeatureServer/0)",
+                     where = where_clause) |> 
+  st_drop_geometry() |> 
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) |>
+  mutate(HashedRecord = "")
+cfs_2024 <- arc_read(r"(https://services1.arcgis.com/UWYHeuuJISiGmgXx/arcgis/rest/services/311_Customer_Service_Requests_2024/FeatureServer/0)",
+                     where = where_clause)
+cfs_2023 <- arc_read(r"(https://services1.arcgis.com/UWYHeuuJISiGmgXx/arcgis/rest/services/311_Customer_Service_Requests_2023/FeatureServer/0)",
+                     where = where_clause)
+cfs_2010_2022 <- lapply(0:12, \(x) try(arc_read(paste0(r"(https://services1.arcgis.com/UWYHeuuJISiGmgXx/ArcGIS/rest/services/311_Customer_Service_Requests_Yearly/FeatureServer/)", x),
+                                                   where = where_clause))) |> bind_rows()
+
+all_cfs <- bind_rows(cfs_2025,
+                     cfs_2024,
+                     cfs_2023,
+                     cfs_2010_2022)
+
+cfs_sample <- sample_frac(all_cfs, 0.10)
+
+ggplot(cfs_sample) +
+  geom_sf() +
+  theme_void()
