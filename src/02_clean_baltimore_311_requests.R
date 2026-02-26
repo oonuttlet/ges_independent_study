9269571805aa21bada7555f99a703a4b73fd134f
oonuttlet <77589230+oonuttlet@users.noreply.github.com>
2026-02-25 22:09:19 -0500

    cleaning steps completed and documented, investigating distributions

--- src/02_clean_baltimore_311_requests.R
+++ src/02_clean_baltimore_311_requests.R
@@ -44,17 +44,25 @@ all_cfs_trim_ws <- all_cfs_trim |>
     )
 
 # ========================================================
-# FILTER DUPLICATE RECORDS (AS MARKED BY BALTIMORE)
+# FILTER DUPLICATE RECORDS (AS MARKED BY BALTIMORE) AND CLEAN DATES
 # ========================================================
 
 all_cfs_nodup <- all_cfs_trim_ws |>
-  dplyr::filter(SRStatus != "Closed (Duplicate)") |> # Filter out records marked as Duplicate by Baltimore Solid Waste
+  dplyr::filter(SRStatus != "Closed (Duplicate)") |>   # Filter out records marked as Duplicate by Baltimore Solid Waste
   dplyr::mutate(
     CloseDate = dplyr::case_when(
-      is.na(CloseDate) ~ StatusDate,                 # If CloseDate is NA, then replace with StatusDate
-      TRUE ~ CloseDate
-    )                                                # Else, if CloseDate is NOT NA, then keep CloseDate
+      is.na(CloseDate) ~ StatusDate,                   # If CloseDate is NA, then replace with StatusDate
+      TRUE ~ CloseDate                                 # Else, if CloseDate is NOT NA, then keep CloseDate
+    ),                                                 
+    StatusDate = dplyr::case_when(
+      StatusDate >= as.Date('2030-01-01') ~ CloseDate, # If StatusDate is too far in the future, then replace with CloseDate
+      TRUE ~ StatusDate                                # Else, if StatusDate is reasonable, then keep StatusDate
+    )
   )
+
+# ========================================================
+# GEOCODE EMPTY GEOMETRIES
+# ========================================================
 
 all_cfs_nodup_empty_geom <- all_cfs_nodup |>
   dplyr::filter(st_is_empty(geom)) # Filter only empty geometries, to be geocoded with MD Multirole Locator
@@ -71,8 +79,100 @@ all_cfs_nodup_empty_bind <- all_cfs_nodup_empty_geom |>
   sf::st_as_sf()                                                     # Ensure output is sf object
 
 all_cfs_nodup_valid_geom <- all_cfs_nodup |>
-  dplyr::filter(!sf::st_is_empty(geom)) |>   # From de-duplicated object, filter rows which have a geometry (saves on memory by avoiding storage)
-  dplyr::bind_rows(all_cfs_nodup_empty_bind) # Bind geocoded rows to end of table to ensure that the entire dataset has valid geometry
+  dplyr::filter(!sf::st_is_empty(geom)) |>                                               # From de-duplicated object, filter rows which have a geometry (saves on memory by avoiding storage)
+  dplyr::bind_rows(all_cfs_nodup_empty_bind) |>                                          # Bind geocoded rows to end of table to ensure that the entire dataset has valid geometry
+  mutate(Longitude = dplyr::case_when(is.na(Longitude) ~ sf::st_coordinates(geom)[,"X"], # Replace NA Longitude with extracted Longitude from geocoded record
+                                      TRUE ~ Longitude),
+         Latitude = dplyr::case_when(is.na(Latitude) ~ sf::st_coordinates(geom)[,"Y"],   # Replace NA Latitude with extracted Latitude from geocoded record
+                                     TRUE ~ Latitude)
+  )
 
 plot(all_cfs_nodup_valid_geom$geom) # Plot geometry to check shape and CfS locations. Most records should have landed back in Baltimore City
+
+# ========================================================
+# INVESTIGATION OF CLEANED DATA
+# ========================================================
+
+library(ggplot2) # Create Elegant Data Visualisations Using the Grammar of Graphics
+options(scipen = 999) # Suppress scientific notation for plotting
+
+# Investigate distribution of date columns
+
+ggplot2::ggplot(all_cfs_nodup_valid_geom) +   # Distribution of CreatedDate
+  ggplot2::geom_bar(aes(x = as.Date(
+    lubridate::floor_date(CreatedDate, "quarter")
+  )), stat = "count") +
+  ggplot2::labs(title = "Distribution of CreatedDate after cleaning", x = "CreatedDate", y = "Count of CfS") +
+  ggplot2::scale_x_date(date_breaks = "2 years") +
+  ggplot2::theme_minimal()
+
+summary(all_cfs_nodup_valid_geom$CreatedDate) # Summary of CreatedDate
+
+ggplot2::ggplot(all_cfs_nodup_valid_geom) +   # Distribution of CloseDate
+  ggplot2::geom_bar(aes(x = as.Date(
+    lubridate::floor_date(CloseDate, "quarter")
+  )), stat = "count") +   
+  ggplot2::labs(title = "Distribution of CloseDate after cleaning", x = "CloseDate", y = "Count of CfS") +
+  ggplot2::scale_x_date(date_breaks = "2 years") +
+  ggplot2::theme_minimal()
+
+summary(all_cfs_nodup_valid_geom$CloseDate)  # Summary of CloseDate
+
+ggplot2::ggplot(all_cfs_nodup_valid_geom) +  # Distribution of StatusDate
+  ggplot2::geom_bar(aes(x = as.Date(
+    lubridate::floor_date(StatusDate, "quarter")
+  )), stat = "count") +  
+  ggplot2::labs(title = "Distribution of StatusDate after cleaning", x = "StatusDate", y = "Count of CfS") +
+  ggplot2::scale_x_date(date_breaks = "2 years") +
+  ggplot2::theme_minimal()
+
+summary(all_cfs_nodup_valid_geom$StatusDate) # Summary of StatusDate
+
+# Investigate distribution of numeric columns
+
+summary(all_cfs_nodup_valid_geom$Latitude)  # Summary of Latitude (should be around 39 for Maryland)
+summary(all_cfs_nodup_valid_geom$Longitude) # Summary of Longitude (should be around -76 for Maryland)
+
+# Investigate distribution of character columns (non-unique)
+
+ggplot2::ggplot(all_cfs_nodup_valid_geom) + 
+  ggplot2::geom_bar(aes(x = SRType), stat = "count") # Distribution of SRType (should contain only SW-Dirty Street and SW-Dirty Alley)
+summary(all_cfs_nodup_valid_geom$SRType)             # Summary of SRType (since character, mostly just a class check)
+sum(is.na(all_cfs_nodup_valid_geom$SRType))          # Count NA values in SRType
+table(all_cfs_nodup_valid_geom$SRType); table(all_cfs_nodup_valid_geom$SRType) / nrow(all_cfs_nodup_valid_geom)
+
+ggplot2::ggplot(all_cfs_nodup_valid_geom) + 
+  ggplot2::geom_bar(aes(x = SRStatus), stat = "count")   # Distribution of SRStatus (should contain only Closed, Closed (Transferred), and New)
+summary(all_cfs_nodup_valid_geom$SRStatus)               # Summary of SRStatus (since character, mostly just a class check)
+sum(is.na(all_cfs_nodup_valid_geom$SRStatus))            # Count NA values in SRStatus
+table(all_cfs_nodup_valid_geom$SRStatus); table(all_cfs_nodup_valid_geom$SRStatus) / nrow(all_cfs_nodup_valid_geom)
+
+ggplot2::ggplot(all_cfs_nodup_valid_geom) +
+  ggplot2::geom_bar(aes(x = lubridate::year(CreatedDate),  # Distribution of Outcome (often null, so not reliable for cleaning)
+                        fill = Outcome), stat = "count") +
+  ggplot2::labs(title = "Temporal distribution of Outcome",
+                x = "Year",
+                y = "Count") +
+  ggplot2::theme_minimal()
+summary(all_cfs_nodup_valid_geom$Outcome)                  # Summary of Outcome (since character, mostly just a class check)
+sum(is.na(all_cfs_nodup_valid_geom$Outcome))               # Count NA values in Outcome
+table(all_cfs_nodup_valid_geom$Outcome); table(all_cfs_nodup_valid_geom$Outcome) / nrow(all_cfs_nodup_valid_geom)
+
+ggplot2::ggplot(all_cfs_nodup_valid_geom) + 
+  ggplot2::geom_bar(aes(x = Agency), stat = "count")  # Distribution of Agency (should always be Solid Waste)
+summary(all_cfs_nodup_valid_geom$Agency)              # Summary of Agency (since character, mostly just a class check)
+sum(is.na(all_cfs_nodup_valid_geom$Agency))           # Count NA values in Agency
+table(all_cfs_nodup_valid_geom$Agency); table(all_cfs_nodup_valid_geom$Agency) / nrow(all_cfs_nodup_valid_geom)
+
+# Check NAs in unique columns
+
+sum(is.na(all_cfs_nodup_valid_geom$SRRecordID))
+table(nchar(all_cfs_nodup_valid_geom$SRRecordID))
+ggplot2::ggplot(all_cfs_nodup_valid_geom) + 
+  ggplot2::geom_bar(aes(x = lubridate::year(CreatedDate), fill = as.factor(nchar(SRRecordID))), stat = "count")
+
+sum(is.na(all_cfs_nodup_valid_geom$ServiceRequestNum))
+table(nchar(all_cfs_nodup_valid_geom$ServiceRequestNum))
+ggplot2::ggplot(all_cfs_nodup_valid_geom) + 
+  ggplot2::geom_bar(aes(x = lubridate::year(CreatedDate), fill = as.factor(nchar(ServiceRequestNum))), stat = "count")
 
